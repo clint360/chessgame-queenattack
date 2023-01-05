128feb917a7ba37145dbcf23b311628c1baf0ea5
clint360 <clint360.rebase@gmail.com>
2023-01-05 12:51:22 +0100

    changed the UI

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,8 +1,8 @@
 let allBoxes = document.querySelectorAll(".boxes") 
 allBoxes.forEach((box) => {
   if (box.textContent != "") {
-    allBoxes.addEventListener('click', {
-
+    box.addEventListener('click', {
+    allBoxes.show
     })
   }
   
